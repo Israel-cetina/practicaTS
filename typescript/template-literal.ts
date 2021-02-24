4cd7c2445a6aacb7492bbb9f9095a9eab06753eb
Israel Cetina <icetina@kwantec.com>
2021-02-24 15:44:01 -0600

    primer commit

---
+++ typescript/template-literal.ts
@@ -0,0 +1,11 @@
+(function () {
+        const nombre = "Israel";
+        const apellido = "Cetina";
+        const edad = 28;
+
+        //concatenando
+        //const salida = nombre + " " + apellido + " ( " +  edad + " )";
+        const salida = `${nombre} ${apellido} (${edad})`; //es mucho más corto y eficiente que concatenar
+
+    }
+)();
---
+++ typescript/tipos.ts
@@ -0,0 +1,13 @@
+(function () {
+        let mensaje: string = "hola";
+        let numero: number = 123;
+        let bool: boolean = true //false
+        let date: Date = new Date();
+
+        let spiderman = {
+            nombre: 'peter',
+            edad: 30
+        };
+
+    }
+)();
